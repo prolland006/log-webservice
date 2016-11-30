43b242f496fffb2d04b587142784784c44307847
Prolland006 <rolland.patrice@gmail.com>
2016-11-30 19:31:37 +0100

    add cors

--- app.js
+++ app.js
@@ -24,14 +24,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+/*
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
+*/
 app.use('/', index);
 app.use('/log', log);
 

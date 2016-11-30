99079da5208ae58c96ffaae2c11fe24371848cec
Prolland006 <rolland.patrice@gmail.com>
2016-11-30 16:56:04 +0100

    bug

--- routes/log.js
+++ routes/log.js
@@ -4,16 +4,6 @@ var router = express.Router();
 
 router.post('/add', function(req,res,next) {
 
-    sakilaDbService.addFilm(req.body.title, (err, data) => {
-        if(err) {
-            //voir si erreur contrainte
-            console.log(err);
-            return next(err);
-        } else {
-            console.log('data',data);
-        }
-    });
-
     console.log(req.body);
 });
 

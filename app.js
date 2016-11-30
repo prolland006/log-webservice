51b0c9462e8e86f3a6ff88a5ce31e5e0dc7ad113
Prolland006 <rolland.patrice@gmail.com>
2016-11-30 18:42:53 +0100

    add cors

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ let corsOptions = {
     preflightContinue: false,
     allowedHeaders: 'Content-Type,Authorization'
 }
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use('/', index);
 app.use('/log', log);

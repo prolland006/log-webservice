ac65891b6fc84a47c07f5b9cef3a240b07cb1f85
Prolland006 <rolland.patrice@gmail.com>
2016-12-02 10:58:29 +0100

    add db param

--- services/log.js
+++ services/log.js
@@ -5,7 +5,8 @@ const DESTINATION_FILE = 2;
 
 //params
 //const DB_URL = 'mongodb://localhost:27017/log';
-const DB_URL = 'mongodb://guest:guest@ds119548.mlab.com:19548/logdb';
+//const DB_URL = 'mongodb://guest:guest@ds119548.mlab.com:19548/logdb';
+const DB_URL = process.env.MONGODB_URL;
 const WRITE_DESTINATION = DESTINATION_MONGODB;
 
 module.exports = class log {
